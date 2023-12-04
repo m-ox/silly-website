297e2095af88f2f99b4b99aea75e6642603b1606
m-ox <maudlin.oxalis@gmail.com>
2023-12-04 00:18:33 -0700

    removed useless import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import useSound from 'use-sound';
 import clickSfx from '../assets/sounds/selection.wav'
 
 function Navbar({currentPage, setPage}) {
